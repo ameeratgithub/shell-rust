d4e22c7b1edc52d566489b5b2b9aac4b1a07a6a4
Ameer Hamza <ameeratwork@gmail.com>
2026-01-24 14:07:04 +0500

    feat: echo command added

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,22 @@ fn main() {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
-        
+
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
-        
+
         let command = command.trim();
         if command == "exit" {
             break;
         }
 
-        println!("{command}: command not found");
+        let args = command.split_whitespace().collect::<Vec<&str>>();
+        let command = args[0];
+        if command == "echo" {
+            let output = &args[1..].join(" ");
+            println!("{output}");
+        } else {
+            println!("{command}: command not found");
+        }
     }
 }
