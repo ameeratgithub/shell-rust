f10029d6d3e8b318f714642a17c167ce3d1d178e
Ameer Hamza <ameeratwork@gmail.com>
2026-01-24 19:44:32 +0500

    fix: executable file name argument fixed

--- src/eval.rs
+++ src/eval.rs
@@ -35,11 +35,11 @@ pub fn run() {
                 if KEYWORDS.contains(args[1]) {
                     println!("{} is a shell builtin", args[1])
                 } else {
-                    check_executable_file_exists_in_paths(&args);
+                    check_executable_file_exists_in_paths(args[1]);
                 }
             }
             _ => {
-                let executable_path = check_executable_file_exists_in_paths(&args);
+                let executable_path = check_executable_file_exists_in_paths(args[0]);
                 if let Some(_) = executable_path {
                     Command::new(args[0]).args(&mut args[1..]).status().unwrap();
                 } else {
@@ -50,23 +50,23 @@ pub fn run() {
     }
 }
 
-fn check_executable_file_exists_in_paths(args: &Vec<&str>) -> Option<String> {
+fn check_executable_file_exists_in_paths(file: &str) -> Option<String> {
     if let Ok(paths) = env::var("PATH") {
         let directories = env::split_paths(&paths);
         for directory in directories {
-            let path = directory.join(args[1]);
+            let path = directory.join(file);
             if path.exists() {
                 let metadata = fs::metadata(&path).unwrap();
                 let mode = metadata.permissions().mode();
                 if mode & 0o111 != 0 {
-                    println!("{} is {}", args[1], &path.display());
+                    println!("{} is {}", file, &path.display());
                     return path.to_str().map(|str| str.to_owned());
                 }
             }
         }
-        println!("{}: not found", args[1]);
+        println!("{}: not found", file);
     } else {
-        println!("{}: not found", args[1]);
+        println!("{}: not found", file);
     }
 
     None
