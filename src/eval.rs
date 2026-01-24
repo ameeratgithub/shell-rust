db938fcb68fe106e113657243128fd52aa13c4e2
Ameer Hamza <ameeratwork@gmail.com>
2026-01-24 19:40:33 +0500

    feat: executable ability added

--- src/eval.rs
+++ src/eval.rs
@@ -1,7 +1,7 @@
 use std::{
     env, fs,
     io::{self, Write},
-    process,
+    process::{self, Command},
 };
 
 use crate::keywords::KEYWORDS;
@@ -20,7 +20,7 @@ pub fn run() {
             break;
         }
 
-        let args = command.split_whitespace().collect::<Vec<&str>>();
+        let mut args = command.split_whitespace().collect::<Vec<&str>>();
         let command = args[0];
 
         match command {
@@ -35,37 +35,39 @@ pub fn run() {
                 if KEYWORDS.contains(args[1]) {
                     println!("{} is a shell builtin", args[1])
                 } else {
-                    if let Ok(paths) = env::var("PATH") {
-                        check_executable_file_exists_in_paths(&paths, &args);
-                    } else {
-                        println!("{}: not found", args[1]);
-                    }
+                    check_executable_file_exists_in_paths(&args);
                 }
             }
             _ => {
-                println!("{command}: command not found");
+                let executable_path = check_executable_file_exists_in_paths(&args);
+                if let Some(_) = executable_path {
+                    Command::new(args[0]).args(&mut args[1..]).status().unwrap();
+                } else {
+                    println!("{command}: command not found");
+                }
             }
         }
     }
 }
 
-fn check_executable_file_exists_in_paths(paths: &String, args: &Vec<&str>) {
-    let directories = env::split_paths(paths);
-    let mut found = false;
-    for directory in directories {
-        let path = directory.join(args[1]);
-        if path.exists() {
-            let metadata = fs::metadata(&path).unwrap();
-            let mode = metadata.permissions().mode();
-            if mode & 0o111 != 0 {
-                println!("{} is {}", args[1], &path.display());
-                found = true;
-                break;
+fn check_executable_file_exists_in_paths(args: &Vec<&str>) -> Option<String> {
+    if let Ok(paths) = env::var("PATH") {
+        let directories = env::split_paths(&paths);
+        for directory in directories {
+            let path = directory.join(args[1]);
+            if path.exists() {
+                let metadata = fs::metadata(&path).unwrap();
+                let mode = metadata.permissions().mode();
+                if mode & 0o111 != 0 {
+                    println!("{} is {}", args[1], &path.display());
+                    return path.to_str().map(|str| str.to_owned());
+                }
             }
         }
-    }
-
-    if !found {
+        println!("{}: not found", args[1]);
+    } else {
         println!("{}: not found", args[1]);
     }
+
+    None
 }
